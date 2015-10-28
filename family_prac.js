93fe4a6c4e923db49cbba2a8efb3ec124bb860c9
liyuqi <am997liyuqi@gmail.com>
2015-10-28 14:58:45 +0800

    Create family_prac.js

---
+++ family_prac.js
@@ -0,0 +1,101 @@
+
+/* //1. insert data
+db.family.insert({"NAME": "全家湖安店","TEL": "02-26591427","POSTel": "02-77136530","px": 121.566979,"py": 25.082946,"addr": "台北市內湖區內湖路一段285巷19號","SERID": 10876.0,"pkey": "013996","post": "114","all": "COFFEE,WiFi,Rest,twoice","road": "內湖路一段","twoice": "Y"    })    
+db.family.insert({"NAME": "全家麗山店","TEL": "02-26279495","POSTel": "02-77230368","px": 121.579098,"py": 25.080373,"addr": "台北市內湖區內湖路一段737巷49號","SERID": 10188.0,"pkey": "011982","post": "114","all": "COFFEE,WiFi,twoice","road": "內湖路一段","twoice": "Y"    })    
+db.family.insert({"NAME": "全家碧湖店","TEL": "02-26277149","POSTel": "02-77230462","px": 121.573314,"py": 25.080997,"addr": "台北市內湖區內湖路一段593號","SERID": 15331.0,"pkey": "011528","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Rest,twoice","road": "內湖路一段","twoice": "Y"    })    
+db.family.insert({"NAME": "全家湖濱店","TEL": "02-26593051","POSTel": "02-77230482","px": 121.580862,"py": 25.083851,"addr": "台北市內湖區內湖路二段103巷104號","SERID": 19222.0,"pkey": "012857","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Toilet,Rest,veg,muffin","road": "內湖路二段","twoice": null    })    
+db.family.insert({"NAME": "全家文湖店","TEL": "02-27973433","POSTel": "02-77096482","px": 121.562634,"py": 25.086887,"addr": "台北市內湖區文湖街59號","SERID": 14540.0,"pkey": "012025","post": "114","all": "SWEETPOTATO,WiFi,icecream,Rest,veg,oneice","road": "文湖街","twoice": null    })    
+db.family.insert({"NAME": "全家紫陽店","TEL": "02-87519703","POSTel": "02-77230436","px": 121.584302,"py": 25.078309,"addr": "台北市內湖區文德路212號","SERID": 14132.0,"pkey": "009191","post": "114","all": "SWEETPOTATO,WiFi,Rest,twoice,muffin","road": "文德路","twoice": "Y"    })    
+db.family.insert({"NAME": "全家德陽店","TEL": "02-27987160","POSTel": "02-77230480","px": 121.57995,"py": 25.076852,"addr": "台北市內湖區文德路22巷25號","SERID": 18963.0,"pkey": "008963","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Rest","road": "文德路","twoice": null    })    
+db.family.insert({"NAME": "全家瑞陽店","TEL": "02-26597471","POSTel": "02-77230422","px": 121.582696,"py": 25.077207,"addr": "台北市內湖區文德路90巷49號","SERID": 12171.0,"pkey": "013050","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Toilet,Rest,muffin","road": "文德路","twoice": null    })    
+db.family.insert({"NAME": "全家三民店","TEL": "02-27942827","POSTel": "02-77230365","px": 121.586051,"py": 25.068786,"addr": "台北市內湖區民權東路六段90巷1號","SERID": 10035.0,"pkey": "010981","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,icecream,Toilet,Rest,oneice","road": "民權東路六段","twoice": null    })    
+db.family.insert({"NAME": "全家內華店","TEL": "02-27911354","POSTel": "02-77230453","px": 121.591812,"py": 25.068665,"addr": "台北市內湖區民權東路六段162號","SERID": 14448.0,"pkey": "009293","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,icecream,Toilet,Rest,veg,oneice","road": "民權東路六段","twoice": null    })    
+db.family.insert({"NAME": "全家金莊店","TEL": "02-27941829","POSTel": "02-77397381","px": 121.587424,"py": 25.062823,"addr": "台北市內湖區石潭路67號","SERID": 54027.0,"pkey": "014027","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Rest","road": "石潭路","twoice": null    })    
+db.family.insert({"NAME": "全家東安店","TEL": "02-26345521","POSTel": "02-77230395","px": 121.615747,"py": 25.067835,"addr": "台北市內湖區安康路480號","SERID": 11264.0,"pkey": "013902","post": "114","all": "COFFEE,SWEETPOTATO,WiFi","road": "安康路","twoice": null    })    
+db.family.insert({"NAME": "全家三總店","TEL": "02-87929407","POSTel": "02-","px": 121.5905,"py": 25.07022,"addr": "台北市內湖區成功路二段325號","SERID": 19090.0,"pkey": "012356","post": "114","all": "COFFEE,oneice","road": "成功路二段","twoice": null    })    
+db.family.insert({"NAME": "全家新內湖店","TEL": "02-27948239","POSTel": "02-77467600","px": 121.589725,"py": 25.071264,"addr": "台北市內湖區成功路二段420號","SERID": 52138.0,"pkey": "012138","post": "114","all": "COFFEE,WiFi,icecream,Rest,oneice","road": "成功路二段","twoice": null    })    
+db.family.insert({"NAME": "全家行善店","TEL": "02-27967192","POSTel": "02-77134249","px": 121.58419,"py": 25.061544,"addr": "台北市內湖區行善路289號","SERID": 52866.0,"pkey": "013637","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,icecream,Toilet,Rest,veg,oneice","road": "行善路","twoice": null    })    
+db.family.insert({"NAME": "全家新東湖店","TEL": "02-26317679","POSTel": "02-77230491","px": 121.61415,"py": 25.069196,"addr": "台北市內湖區東湖路43巷4號","SERID": 50301.0,"pkey": "014327","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Rest,veg,twoice","road": "東湖路","twoice": "Y"    })    
+db.family.insert({"NAME": "全家金瑞店","TEL": "02-27934276","POSTel": "02-77231651","px": 121.586693,"py": 25.088283,"addr": "台北市內湖區金龍路224號","SERID": 51866.0,"pkey": "013688","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,icecream,Toilet,Rest,veg,oneice","road": "金龍路","twoice": null    })    
+db.family.insert({"NAME": "全家星湖店","TEL": "02-27906271","POSTel": "02-77463097","px": 121.596255,"py": 25.080855,"addr": "台北市內湖區星雲街161巷1號","SERID": 52945.0,"pkey": "014282","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,icecream,Rest,oneice","road": "星雲街","twoice": null    })    
+db.family.insert({"NAME": "全家紫星店","TEL": "02-87929052","POSTel": "02-77137287","px": 121.592844,"py": 25.079933,"addr": "台北市內湖區康寧路一段79號","SERID": 51481.0,"pkey": "013870","post": "114","all": "SWEETPOTATO,WiFi,icecream,Rest","road": "康寧路一段","twoice": null    })    
+db.family.insert({"NAME": "全家護校店","TEL": "02-26342271","POSTel": "02-77230479","px": 121.61003,"py": 25.075208,"addr": "台北市內湖區康寧路三段75巷137號","SERID": 18938.0,"pkey": "013798","post": "114","all": "COFFEE,WiFi,icecream,oneice,muffin","road": "康寧路三段","twoice": null    })    
+db.family.insert({"NAME": "全家康福店","TEL": "02-26339860","POSTel": "02-77230455","px": 121.618828,"py": 25.069632,"addr": "台北市內湖區康樂街87號","SERID": 14453.0,"pkey": "014160","post": "114","all": "COFFEE,WiFi,Rest,twoice,muffin","road": "康樂街","twoice": "Y"    })    
+db.family.insert({"NAME": "全家精英店","TEL": "02-26579148","POSTel": "02-77230475","px": 121.566762,"py": 25.07824,"addr": "台北市內湖區堤頂大道二段241號","SERID": 16748.0,"pkey": "014042","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Rest,muffin","road": "堤頂大道二段","twoice": null    })    
+db.family.insert({"NAME": "全家新瑞店","TEL": "02-26592842","POSTel": "02-77230463","px": 121.563774,"py": 25.08115,"addr": "台北市內湖區堤頂大道二段407巷26號","SERID": 15377.0,"pkey": "009711","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Rest,twoice,muffin","road": "堤頂大道二段","twoice": "Y"    })    
+db.family.insert({"NAME": "全家港墘店","TEL": "02-87972796","POSTel": "02-77230393","px": 121.578066,"py": 25.082132,"addr": "台北市內湖區港墘路44號","SERID": 11175.0,"pkey": "010282","post": "114","all": "COFFEE,SWEETPOTATO,WiFi","road": "港墘路","twoice": null    })    
+db.family.insert({"NAME": "全家興盛店","TEL": "02-27903667","POSTel": "02-77230400","px": 121.585316,"py": 25.057421,"addr": "台北市內湖區新明路182號","SERID": 11482.0,"pkey": "012756","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,veg,twoice","road": "新明路","twoice": "Y"    })    
+db.family.insert({"NAME": "全家新湖店","TEL": "02-27937065","POSTel": "02-77230433","px": 121.582544,"py": 25.064886,"addr": "台北市內湖區新湖二路250巷8號","SERID": 12915.0,"pkey": "009697","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,icecream,Rest,oneice","road": "新湖二路","twoice": null    })    
+db.family.insert({"NAME": "全家瑞光店","TEL": "02-87514993","POSTel": "02-77230425","px": 121.567655,"py": 25.079464,"addr": "台北市內湖區瑞光路510號","SERID": 12253.0,"pkey": "008784","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Toilet,Rest,twoice","road": "瑞光路","twoice": "Y"    })    
+db.family.insert({"NAME": "全家瑞湖店","TEL": "02-87524016","POSTel": "02-77230468","px": 121.570252,"py": 25.080095,"addr": "台北市內湖區港墘里瑞光路513巷39號","SERID": 16082.0,"pkey": "013730","post": "114","all": "SWEETPOTATO,Photo,WiFi,Rest,twoice,muffin","road": "瑞光路","twoice": "Y"    })    
+db.family.insert({"NAME": "全家環山店","TEL": "02-26271008","POSTel": "02-77230411","px": 121.565673,"py": 25.082804,"addr": "台北市內湖區環山路一段5號","SERID": 11864.0,"pkey": "009709","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,twoice","road": "環山路一段","twoice": "Y"    })    
+db.family.insert({"NAME": "全家新德明店","TEL": "02-27996853","POSTel": "02-77463246","px": 121.567981,"py": 25.086553,"addr": "台北市內湖區環山路一段110號","SERID": 53284.0,"pkey": "013284","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,icecream,Rest,oneice","road": "環山路一段","twoice": null    })    
+db.family.insert({"NAME": "全家湖山店","TEL": "02-26572616","POSTel": "02-77230410","px": 121.573544,"py": 25.082171,"addr": "台北市內湖區麗山街348巷10弄2號","SERID": 11845.0,"pkey": "010473","post": "114","all": "COFFEE,SWEETPOTATO,WiFi,Rest","road": "麗山街","twoice": null    }  ])
+*/
+
+/*
+// 1.2 data ETL(loc.coordinate, tags.split(','))
+var family_src = db.getCollection('family').find();
+
+var keys = [];
+for(var k in family_src[0]) keys.push(k);
+for(var k in keys){ print(keys[k]);}
+
+//printjson(family_src[0]);
+var i =0;
+var family_loc = db.getCollection('family').find({}).forEach(function (doc) {
+    var service=[];
+    var coordinates = [];
+    coordinates.push(doc.px);
+    coordinates.push(doc.py);
+
+    service = doc.all.split(',');
+    doc.service = service;
+    doc.loc = {
+           type : 'Point',
+           coordinates : coordinates
+       };
+    if(i<5)
+        printjson(doc);
+    i++;
+    db.getCollection('family_loc').insert(doc);
+});
+
+*/
+
+//2. check coordinate
+db.family_loc.find({},{_id:0,'loc.coordinate':1})
+
+//3. index coordinate
+db.family_loc.ensureIndex({tags:1,loc:'2dsphere'})
+
+//4.1 find in box
+db.family_loc.find({
+    loc: {$geoWithin:{$box:[ //bottom_left, top_right
+        [121.584070 ,25.0758137],
+        [121.618828 ,25.0882830]
+    ]}}
+})
+
+//4.2 find in Polygon
+db.family_loc.find({
+    loc: {$geoWithin:{$geometry:{
+        type:"Polygon", // begin, end
+        coordinates:[[
+        [121.5840702,25.0882830],
+        [121.6188280,25.0882830],
+        [121.6188280,25.0758137],
+        [121.5840702,25.0758137],
+        [121.5840702,25.0882830]
+        ]]
+    }}}
+})
+
+//4.3 find in circle
+db.family_loc.find({ 
+    service : {$in:['Toilet']},
+    loc: {$geoWithin:{$center:[
+        [121.6188280,25.0758137],
+        0.03
+    ]}}
+})
